b5fdd436420c463dc6f20de9210c20673e8791ea
Clayton R. Veras <veras.clayton@gmail.com>
2016-01-04 15:46:09 -0200

    First commit

---
+++ www/js/controller/LoginCtrl.js
@@ -0,0 +1,17 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+angular.module("workstops").controller("LoginCtrl", function($scope){
+    
+    $scope.loginData = [];
+    
+    this.doLogin = function (){
+        
+    };
+    
+});
+
+
