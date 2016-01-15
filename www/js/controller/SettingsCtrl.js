7b0c2c78f98f849c20fef58205f8bb9036342a4b
clayton <clayton@clayton-Aspire>
2016-01-15 01:08:42 -0200

    Tela de configuraçao com botoes de escolha da carga horaria diaria. Salvando as configuraçoes em localstorage.

--- www/js/controller/SettingsCtrl.js
+++ www/js/controller/SettingsCtrl.js
@@ -1,9 +1,37 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-angular.module("workstops").controller("SettingsCtrl", function(){
+angular.module("workstops").controller("SettingsCtrl", function($scope, $localstorage){
+    
+    init();
+    var lastChanged;
+    
+    function init(){
+        $scope.data = {
+          workload : null,
+          workloadOptions: [
+              {number: 1},
+              {number: 2},
+              {number: 3},
+              {number: 4},
+              {number: 5},
+              {number: 6},
+              {number: 7},
+              {number: 8}
+          ] 
+        };
+    };
+    
+    $scope.saveWorkload = function (value){
+      $localstorage.setObject("workload",value);  
+    };
+    
+    $scope.changeColor = function (id){
+        if(!$localstorage.isEmpty(lastChanged)){
+            document.getElementById(lastChanged).style.borderColor="rgb(220, 220, 220)";
+            document.getElementById(id).style.borderColor="#73AD21";
+            lastChanged = id;
+        }else {
+            document.getElementById(id).style.borderColor="#73AD21";
+            lastChanged = id;
+        }
+    };
     
 });
